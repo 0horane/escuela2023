fe9897366bc798c786baceffd7a96618a81f1308
0horane <lehorane@gmail.com>
2023-06-12 16:40:45 -0300

    sin terminar chat germade

--- adm_sist_redes/Germade chat/client.py
+++ adm_sist_redes/Germade chat/client.py
@@ -1,7 +1,12 @@
 import socket
 
 
-sock=socket.socket(family=AF_INET, type=SOCK_STREAM)
-sick.bind('127.0.0.1', 8080)
-sock.listen()
-print(sock.accept())
+sock=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 8080))
+sock.sendall(b"tex=st")
+#print(sock.accept())
+
+#https://realpython.com/python-sockets/#echo-client
+#https://docs.python.org/3/library/threading.html
+#https://docs.python.org/3/library/socket.html
+#https://paulbutler.org/2022/what-does-it-mean-to-listen-on-a-port/
--- adm_sist_redes/Germade chat/server.py
+++ adm_sist_redes/Germade chat/server.py
@@ -1,8 +1,13 @@
 
 import socket
+import threading
 
-
-sock=socket.socket(family=AF_INET, type=SOCK_STREAM)
-sick.bind('127.0.0.1', 8080)
+# AF_INET = IPv4
+# SOCK_STREAM = TCP
+sock=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+# 127.0.0.1 para que solo sea accesible localmente
+sock.bind(('0.0.0.0', 8080))
 sock.listen()
-print(sock.accept())
+#sock.setblocking(False)
+conn,addr=sock.accept()
+print(sock.recv(1024))
