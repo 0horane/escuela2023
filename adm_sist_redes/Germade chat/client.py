afae4400a2d93c9581e2bd7efb76a5d716abb9b4
0horane <lehorane@gmail.com>
2023-06-15 07:55:25 -0300

    mas en servidor

--- adm_sist_redes/Germade chat/client.py
+++ adm_sist_redes/Germade chat/client.py
@@ -1,10 +1,15 @@
 import socket
+import threading
 
+def thread_run(sock):
+    while True:
+        print(sock.recv(1024))
 
 sock=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 8080))
-sock.sendall(b"tex=st")
-#print(sock.accept())
+while True:
+    sock.sendall(bytes(input(), "utf-8"))
+#print(sock.recv(1024))
 
 #https://realpython.com/python-sockets/#echo-client
 #https://docs.python.org/3/library/threading.html
--- adm_sist_redes/Germade chat/server.py
+++ adm_sist_redes/Germade chat/server.py
@@ -2,6 +2,22 @@
 import socket
 import threading
 
+def thread_run(conn):
+    while True:
+        print(conn.recv(1024))
+
+def input_thread_run(socketlist):
+    while True:
+        sendtext=input()
+        print(sendtext)
+        for item in socketlist:
+            print(item)
+            item.sendall(bytes(sendtext, "utf-8"))
+
+
+
+
+
 # AF_INET = IPv4
 # SOCK_STREAM = TCP
 sock=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
@@ -9,5 +25,14 @@ sock=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 8080))
 sock.listen()
 #sock.setblocking(False)
-conn,addr=sock.accept()
-print(sock.recv(1024))
+connlist=[]
+datalist_lock=threading.Lock()
+datalist=[]
+while True:
+    conn,addr=sock.accept()
+    connlist.append(conn)
+    tthread=threading.Thread(target=thread_run, args=(conn,), daemon=True)
+    tthread.start()
+
+    inthread=threading.Thread(target=input_thread_run, args=(connlist,), daemon=True)
+    inthread.start()
